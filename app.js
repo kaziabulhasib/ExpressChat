05f298b9b647f1dc5fd6bc54deb27600610403dd
kaziabulhasib <hasibkazi420@gmail.com>
2025-08-28 22:04:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+dotenv.config();
+
+// db connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => console.log("mongodb connection successful"))
+  .catch((err) => console.log(err));
+
+// request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.listen(process.env.PORT, () => {
+  console.log("app is running on port", process.env.PORT);
+});
