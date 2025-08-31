7bfcc567e28e70ee7e9873778cc5c32924090e8c
kaziabulhasib <hasibkazi420@gmail.com>
2025-08-31 13:57:47 +0530

    feat :  user add & validation func

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,11 +1,18 @@
 const express = require("express");
+
+// internal imports
 const { getUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const { addUserValidators } = require("../middlewares/users/userValidation");
 
 const router = express.Router();
 
 // users page
 
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post("/", avatarUpload, addUserValidators);
 
 module.exports = router;
